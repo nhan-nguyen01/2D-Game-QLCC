5d0e5572d9d66e33090b3b5acc9570f6a414abd0
nhan-nguyen01 <bestysplays69@gmail.com>
2023-02-19 19:57:25 +0700

    Sửa cơ chế nhảy của player
    Chỉ cho player nhảy khi đứng ở trên mặt đất

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,9 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
-
+    private BoxCollider2D coll;
     private Animator anim;
     private float dirX = 0f;
+    [SerializeField] private LayerMask jumpableGround;
     [SerializeField] private float MoveSpeed = 7f;
     [SerializeField] private float JumpForce = 14f;
 
@@ -19,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -26,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && IsGrounded())
             {
             rb.velocity = new Vector2(rb.velocity.x, 14f); 
             }
@@ -58,5 +60,9 @@ public class PlayerMovement : MonoBehaviour
                 state = MovementState.fall;
             }
         anim.SetInteger("state", (int)(state));
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
